f1a92cdadbd67e19cedd493f919a36fc22ce4213
Angie Bai <anqib48356@foxmail.com>
2014-06-22 22:40:27 +0800

    updated kind of clean data

--- trynltk.py
+++ trynltk.py
@@ -18,14 +18,62 @@ for line in fileread.readlines():
         if(line=="\n" and flag==1): #这种情况说明是段末
             flag=0
             string=' '.join(myset) #将一段话合成一个string
-            '''正则表达式对一段话转成的句子进行处理，去掉不需要的部分，注意这里可以添加其它的正则表达式进行替换'''
-            pattern=re.compile(r'\[.*\]') 
-            string=pattern.sub(r'',string) #用设定的正则表达式对string进行处理，这里使用的是去掉'[]'及其中的内容
+            pattern1=re.compile(r'Mr\.') 
+            string=pattern1.sub(r'Mr&',string) #后面还需要换回来
+
+            pattern1_=re.compile(r'Mrs\.') 
+            string=pattern1_.sub(r'Mrs&',string)
+            
+            pattern2=re.compile(r'(\.|\!|\?)\"')
+            string=pattern2.sub(r'',string)     #直接去掉就不会被分开
+
+            pattern3=re.compile(r' - ')
+            string=pattern3.sub(r' ',string)                   
+
+            pattern4=re.compile(r'\[.*\]') 
+            string=pattern4.sub(r'',string)
             
             sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
             sents = sent_detector.tokenize(string.strip())
            
             for sen in sents:
+			   
+                pattern5=re.compile(r'(D|d)on\'t')
+                sen=pattern5.sub(r'do not',sen)
+
+                pattern6=re.compile(r'(C|c)an\'t')
+                sen=pattern6.sub(r'can not',sen)
+
+                pattern7=re.compile(r'(W|w)on\'t')
+                sen=pattern7.sub(r'will not',sen)
+
+                pattern8=re.compile(r'(D|d)oesn\'t')
+                sen=pattern8.sub(r'does not',sen)
+
+                pattern9=re.compile(r'(W|w)e\'re')
+                sen=pattern9.sub(r'we are',sen)
+                    
+                pattern10=re.compile(r'(W|w)e\'ll')
+                sen=pattern10.sub(r'we will',sen)
+
+                pattern11=re.compile(r'(I|i)\'ve')
+                sen=pattern11.sub(r'I have',sen)
+                    
+                pattern12=re.compile(r'(L|l)et\'s')
+                sen=pattern12.sub(r'let us',sen)
+
+                pattern13=re.compile(r'Mr\&') 
+                sen=pattern13.sub(r'Mr ',sen)
+
+                pattern13_=re.compile(r'Mrs\&') 
+                sen=pattern13_.sub(r'Mrs ',sen)
+
+                pattern14=re.compile(r'\'s') 
+                sen=pattern14.sub(r' s',sen)
+
+                #除了字母，其它所有的符号都删除
+                sen=filter(lambda ch:ch in 'qwertyuiopasdfghjklzxcvbnm QWERTYUIOPASDFGHJKLZXCVBNM',sen)		
+				
                 filewrite.write(sen)
                 filewrite.write('\n')
                 #print sen
